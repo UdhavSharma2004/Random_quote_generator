330d77d3ee8c03a73b902fb4952287d2c1493b12
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2022-12-20 00:21:44 +0530

    responsiveness added + added fetch API

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-const quoteobj=[
-    ["Start writing, no matter what. The water does not flow until the faucet is turned on."],
-    ["Get it down. Take chances. It may be bad, but it's the only way you can do anything really good."],
-    ["The first draft is just you telling yourself the story."],
-    ["You can always edit a bad page. You can't edit a blank page"],
-    ["The most valuable of all talents is that of never using two words when one will do. "]
-]
-const writerobj=[
-    ["- Louis L'Amour"],
-    ["- William Faulkner"],
-    ["- Terry Pratchett"],
-    ["- Jodi Picoult"],
-    ["- Thomas Jefferson"]
-]
+const url = "https://api.quotable.io/random";
+
 let qid=document.getElementById("qte");
 let wid=document.getElementById("wrtr");
 let bid=document.getElementById("btn");
 
 bid.addEventListener("click",function(){
-    let ran= Math.floor(Math.random()*quoteobj.length);
-    qid.innerText=quoteobj[ran];
-    wid.innerText=writerobj[ran];
+    fetch(url).then((data) => data.json()).then((item)=>{
+        qid.innerText=item.content;
+        wid.innerText=item.author;
+    });
 });
